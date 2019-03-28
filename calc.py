92ca00a543a26b79a112f3355ad056aacbcbbc62
Bruno Costa <bruno@MacBook-Pro-de-Ana-2.local>
2019-03-28 15:05:39 -0300

    Todas corrigidas

--- calc.py
+++ calc.py
@@ -1,13 +1,24 @@
+import math
+import numpy as np
 from classes import Point
 from classes import Element
 from classes import Load
-from pars import pars
-import numpy as np
+from classes import Rigidez
+from parser import pars
+from functions import calcSenCos
+from functions import matrixMaker
+from functions import superMatrixMaker
 
 # Arquivo de entrada
 arquivo = "entrada.txt"
 
 point_list, element_list, load_list = pars(arquivo)
+
+element_list, point_list = calcSenCos(element_list, point_list)
+matrix_list = []
+for i in element_list:
+    matrix_list.append(matrixMaker(i))
+superMatrix = superMatrixMaker(matrix_list,3)
 
 v_carregamento = []
 for e in load_list:
@@ -24,4 +35,4 @@ for e in load_list:
         v_carregamento.append(temp)
 
 v_carregamento = np.array(v_carregamento)
-print(v_carregamento)
+#print(v_carregamento)
